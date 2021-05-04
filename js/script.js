2e570f86d49a216d3faaceaca2462b5a3f78902d
Wojciech Bylica <wojtekbyl@gmail.com>
2021-05-04 16:17:24 +0200

    js names and functions corrected

--- js/script.js
+++ js/script.js
@@ -18,56 +18,48 @@
     const questionNineth = document.querySelector(".js-question__nineth");
     const questionTenth = document.querySelector(".js-question__tenth");
 
-    const questionNumber = () => {
+    const showNextQuestion = () => {
         switch (questionIndex) {
             case 1:
                 questionFirst.classList.add("form__element--hidden");
                 questionSecond.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 2:
                 questionSecond.classList.add("form__element--hidden");
                 questionThird.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 3:
                 questionThird.classList.add("form__element--hidden");
                 questionFourth.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 4:
                 questionFourth.classList.add("form__element--hidden");
                 questionFifth.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 5:
                 questionFifth.classList.add("form__element--hidden");
                 questionSixth.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 6:
                 questionSixth.classList.add("form__element--hidden");
                 questionSeventh.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 7:
                 questionSeventh.classList.add("form__element--hidden");
                 questionEightth.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 8:
                 questionEightth.classList.add("form__element--hidden");
                 questionNineth.classList.remove("form__element--hidden");
-                questionIndex++;
                 break;
             case 9:
                 questionNineth.classList.add("form__element--hidden");
                 questionTenth.classList.remove("form__element--hidden");
                 sendButton.classList.remove("form__element--hidden");
                 nextButton.classList.add("form__element--hidden");
-                questionIndex++;
                 break;
         }
+        questionIndex++;
     };
 
     const showSendButton = () => {
@@ -76,60 +68,61 @@
         answerButton.classList.remove("form__element--hidden");
     }
 
-    const q1True = document.querySelector(".js-q1__true");
-    const q2True = document.querySelector(".js-q2__true");
-    const q3True = document.querySelector(".js-q3__true");
-    const q4True = document.querySelector(".js-q4__true");
-    const q5True = document.querySelector(".js-q5__true");
-    const q6True = document.querySelector(".js-q6__true");
-    const q7True = document.querySelector(".js-q7__true");
-    const q8True = document.querySelector(".js-q8__true");
-    const q9True = document.querySelector(".js-q9__true");
-    const q10True = document.querySelector(".js-q10__true");
+    const correctAnswerToQuestion1 = document.querySelector(".js-q1__true");
+    const correctAnswerToQuestion2 = document.querySelector(".js-q2__true");
+    const correctAnswerToQuestion3 = document.querySelector(".js-q3__true");
+    const correctAnswerToQuestion4 = document.querySelector(".js-q4__true");
+    const correctAnswerToQuestion5 = document.querySelector(".js-q5__true");
+    const correctAnswerToQuestion6 = document.querySelector(".js-q6__true");
+    const correctAnswerToQuestion7 = document.querySelector(".js-q7__true");
+    const correctAnswerToQuestion8 = document.querySelector(".js-q8__true");
+    const correctAnswerToQuestion9 = document.querySelector(".js-q9__true");
+    const correctAnswerToQuestion10 = document.querySelector(".js-q10__true");
 
     const result = document.querySelector(".js-result");
 
+    const countResults = () => {
+        return correctAnswerToQuestion1.checked +
+            correctAnswerToQuestion2.checked +
+            correctAnswerToQuestion3.checked +
+            correctAnswerToQuestion4.checked +
+            correctAnswerToQuestion5.checked +
+            correctAnswerToQuestion6.checked +
+            correctAnswerToQuestion7.checked +
+            correctAnswerToQuestion8.checked +
+            correctAnswerToQuestion9.checked +
+            correctAnswerToQuestion10.checked;
+    }
 
-    const countGoodAnswers = (event) => {
+    const showResults = (event) => {
         event.preventDefault();
 
-        const sum = (
-            q1True.checked +
-            q2True.checked +
-            q3True.checked +
-            q4True.checked +
-            q5True.checked +
-            q6True.checked +
-            q7True.checked +
-            q8True.checked +
-            q9True.checked +
-            q10True.checked
-        );
+        const sum = countResults();
         result.innerText = `${sum}/10 pkt.`;
     }
 
-    const formInputTrue = document.querySelector(".form__input--true");
-    const formInputTrue2 = document.querySelector(".form__input--true2");
-    const formInputTrue3 = document.querySelector(".form__input--true3");
-    const formInputTrue4 = document.querySelector(".form__input--true4");
-    const formInputTrue5 = document.querySelector(".form__input--true5");
-    const formInputTrue6 = document.querySelector(".form__input--true6");
-    const formInputTrue7 = document.querySelector(".form__input--true7");
-    const formInputTrue8 = document.querySelector(".form__input--true8");
-    const formInputTrue9 = document.querySelector(".form__input--true9");
-    const formInputTrue10 = document.querySelector(".form__input--true10");
+    const goodAnswerToQuestion = document.querySelector(".form__input--true");
+    const goodAnswerToQuestion2 = document.querySelector(".form__input--true2");
+    const goodAnswerToQuestion3 = document.querySelector(".form__input--true3");
+    const goodAnswerToQuestion4 = document.querySelector(".form__input--true4");
+    const goodAnswerToQuestion5 = document.querySelector(".form__input--true5");
+    const goodAnswerToQuestion6 = document.querySelector(".form__input--true6");
+    const goodAnswerToQuestion7 = document.querySelector(".form__input--true7");
+    const goodAnswerToQuestion8 = document.querySelector(".form__input--true8");
+    const goodAnswerToQuestion9 = document.querySelector(".form__input--true9");
+    const goodAnswerToQuestion10 = document.querySelector(".form__input--true10");
 
-    const showGoodAnswers = () => {
-        formInputTrue.classList.toggle("form__input--showTrue");
-        formInputTrue2.classList.toggle("form__input--showTrue");
-        formInputTrue3.classList.toggle("form__input--showTrue");
-        formInputTrue4.classList.toggle("form__input--showTrue");
-        formInputTrue5.classList.toggle("form__input--showTrue");
-        formInputTrue6.classList.toggle("form__input--showTrue");
-        formInputTrue7.classList.toggle("form__input--showTrue");
-        formInputTrue8.classList.toggle("form__input--showTrue");
-        formInputTrue9.classList.toggle("form__input--showTrue");
-        formInputTrue10.classList.toggle("form__input--showTrue");
+    const serveTheSummary = () => {
+        goodAnswerToQuestion.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion2.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion3.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion4.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion5.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion6.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion7.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion8.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion9.classList.toggle("form__input--showTrue");
+        goodAnswerToQuestion10.classList.toggle("form__input--showTrue");
 
         questionFirst.classList.remove("form__element--hidden");
         questionSecond.classList.remove("form__element--hidden");
@@ -142,21 +135,21 @@
         questionNineth.classList.remove("form__element--hidden");
         questionTenth.classList.remove("form__element--hidden");
 
-        answerButton.innerText = answerButton.innerText === "Podświetl prawidłowe odpowiedzi" ? "Ukryj podświetlenia" : "Podświetl prawidłowe odpowiedzi";
+        answerButton.innerText = goodAnswerToQuestion.classList.contains("form__input--showTrue") ? "Ukryj podświetlenia" : "Podświetl prawidłowe odpowiedzi";
     }
 
     const resetList = () => {
         result.innerText = "";
-        formInputTrue.classList.remove("form__input--showTrue");
-        formInputTrue2.classList.remove("form__input--showTrue");
-        formInputTrue3.classList.remove("form__input--showTrue");
-        formInputTrue4.classList.remove("form__input--showTrue");
-        formInputTrue5.classList.remove("form__input--showTrue");
-        formInputTrue6.classList.remove("form__input--showTrue");
-        formInputTrue7.classList.remove("form__input--showTrue");
-        formInputTrue8.classList.remove("form__input--showTrue");
-        formInputTrue9.classList.remove("form__input--showTrue");
-        formInputTrue10.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion2.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion3.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion4.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion5.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion6.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion7.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion8.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion9.classList.remove("form__input--showTrue");
+        goodAnswerToQuestion10.classList.remove("form__input--showTrue");
 
         questionFirst.classList.remove("form__element--hidden");
         questionSecond.classList.add("form__element--hidden");
@@ -181,14 +174,14 @@
     const init = () => {
         welcome();
 
-        nextButton.addEventListener("click", questionNumber);
+        nextButton.addEventListener("click", showNextQuestion);
 
         sendButton.addEventListener("click", showSendButton);
 
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", countGoodAnswers);
+        form.addEventListener("submit", showResults);
 
-        answerButton.addEventListener("click", showGoodAnswers);
+        answerButton.addEventListener("click", serveTheSummary);
 
         const resetButton = document.querySelector(".js-reset__button")
         resetButton.addEventListener("click", resetList);
